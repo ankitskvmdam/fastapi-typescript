0475c505fb89db23308311781d9b3be831b709dd
Ankit Kumar <ankits.kvmdam@hotmail.com>
2025-12-11 00:22:24 +0900

    feat: add cors

--- server/main.py
+++ server/main.py
@@ -1,27 +1,36 @@
 from datetime import datetime, timezone
 
 from fastapi import FastAPI
-from middleware import RequestLoggerMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+
+from .middleware import RequestLoggerMiddleware
 
 app = FastAPI()
 
 app.add_middleware(RequestLoggerMiddleware)
 
+# To support frontend adding cors.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-class SummariesRequest(BaseModel):
-    """Summaries request."""
 
+class SummaryRequest(BaseModel):
     text: str
 
 
 @app.post("/summaries")
-async def summaries(request: SummariesRequest):
+async def summaries(request: SummaryRequest):
     result = request.text.split(None, 10)
 
     return {
         "summary": " ".join(result[:10]),
-        "timestamp": datetime.now(tz=timezone.utc),
+        "timestamp": datetime.now(tz=timezone.utc).isoformat(),
     }
 
 
